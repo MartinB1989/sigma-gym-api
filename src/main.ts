1f15ecb3ae4a809ea7f91224dc66cb098d05b1a3
MartinB <mbenitez@zecat.com>
2024-12-30 09:58:32 -0300

    retorna info de usuario

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,17 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v1')
+  
+  // Habilitar CORS
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT ?? 3000);
-  console.log(`Server is running on port ${process.env.PORT ?? 3000}`);
+  await app.listen(process.env.PORT ?? 3001);
+  console.log(`Server is running on port ${process.env.PORT ?? 3001}`);
 }
 bootstrap();
